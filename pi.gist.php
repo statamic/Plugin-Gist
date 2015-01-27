ceeb6ae1c9f8037db4644f6c312abd444fd65648
brettdewoody <brett@digitalwaxworks.com>
2015-01-27 00:08:08 -0700

    Fix issue where {{ gist:id }} does not work.
    Fixes issue #4

--- pi.gist.php
+++ pi.gist.php
@@ -8,13 +8,13 @@ class Plugin_gist extends Plugin {
     'author_url' => 'http://jackmcdade.com'
   );
 
-  static public function __callStatic($method, $args) {
+  public function __call($method, $args) {
     return "<script src=\"http://gist.github.com/{$method}.js\"></script>";
   }
 
   public function index() {
-  	$id = $this->fetchParam('id', '');
-  	$file = $this->fetchParam('file', '');
+    $id = $this->fetchParam('id', '');
+    $file = $this->fetchParam('file', '');
 
     return "<script src=\"http://gist.github.com/{$id}.js" . ($file == '' ? '' : '?file=' . $file) . "\"></script>";
   }
